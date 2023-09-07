a6bf13eeeb1b8c73b2bf6d472d832938f4459b2b
MelonPult0 <tmx9903@gmail.com>
2023-09-07 12:28:50 -0500

    Add error handling

--- src/aws.ts
+++ src/aws.ts
@@ -8,18 +8,36 @@ const { DynamoDB } = AWS;
 const dynamodb = new DynamoDB();
 
 // 1 -  Create a table
-export const dynamodbCreateTable = async (
-  params: AWS.DynamoDB.CreateTableInput
-) => {
+export const dynamodbCreateTable = async (tableName: string) => {
+  const vendorsTestTableParams: AWS.DynamoDB.CreateTableInput = {
+    TableName: tableName,
+    KeySchema: [{ AttributeName: 'twitterId', KeyType: 'HASH' }],
+    AttributeDefinitions: [
+      { AttributeName: 'twitterId', AttributeType: 'S' },
+    ],
+    ProvisionedThroughput: {
+      ReadCapacityUnits: 10,
+      WriteCapacityUnits: 10,
+    },
+  };
   try {
-    const res = await dynamodb.createTable(params).promise();
+    const res = await dynamodb.createTable(vendorsTestTableParams).promise();
     console.log('Table created', res);
     return res;
-  } catch (error) {
-    console.error(error);
-    throw new Error('dynamodbCreateTable error');
+  } catch (error: any) {
+    if (error.code === 'ResourceInUseException') {
+      console.log('------------------------------------------------------');
+      console.log('Table already existed.');
+      console.log('------------------------------------------------------');
+      dynamodbDescribeTable(tableName);
+    } else {
+      console.error(error);
+      throw new Error('dynamodbCreateTable error');
+    }
   }
 };
+
+
 
 // 2 - Describe a table
 export const dynamodbDescribeTable = async (tableName: string) => {
@@ -29,9 +47,16 @@ export const dynamodbDescribeTable = async (tableName: string) => {
       .promise();
     console.log('Table retrieved', res);
     return res;
-  } catch (error) {
-    console.error(error);
-    throw new Error('dynamodbDescribeTable error');
+  } catch (error: any) {
+    if (error.code === 'ResourceNotFoundException') {
+      console.log('------------------------------------------------------');
+      console.log('Table doesn\'t exist. Preparing to create a new table.');
+      console.log('------------------------------------------------------');
+      dynamodbCreateTable(tableName);
+    } else {
+      console.error(error);
+      throw new Error('dynamodbDescribeTable error');
+    }
   }
 };
 
@@ -43,8 +68,15 @@ export const dynamodbDeleteTable = async (tableName: string) => {
       .promise();
     console.log('Table deleted', res);
     return res;
-  } catch (error) {
-    console.error(error);
-    throw new Error('dynamodbDeleteTable error');
+  } catch (error: any) {
+    if (error.code === 'ResourceNotFoundException') {
+      console.log('------------------------------------------------------');
+      console.log('Table doesn\'t exist.');
+      console.log('------------------------------------------------------');
+    } else {
+      console.error(error);
+      throw new Error('dynamodbDeleteTable error');
+    }
+    
   }
 };
--- src/index.ts
+++ src/index.ts
@@ -6,21 +6,9 @@ import {
 
 const init = async () => {
   const TABLE_NAME_CONST = 'vendors';
-
-  const vendorsTestTableParams: AWS.DynamoDB.CreateTableInput = {
-    TableName: 'vendors',
-    KeySchema: [{ AttributeName: 'twitterId', KeyType: 'HASH' }],
-    AttributeDefinitions: [
-      { AttributeName: 'twitterId', AttributeType: 'S' },
-    ],
-    ProvisionedThroughput: {
-      ReadCapacityUnits: 10,
-      WriteCapacityUnits: 10,
-    },
-  };
   
   // 1 - create table
-    // dynamodbCreateTable(vendorsTestTableParams);
+    // dynamodbCreateTable(TABLE_NAME_CONST);
   
   // 2 - describe table
     // dynamodbDescribeTable(TABLE_NAME_CONST);
